ab1fc23584538f24d29ee6d00040bc53a061aa49
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-09-14 15:22:21 +0500

    keep on

---
+++ N37Home1/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N37Home1.Models
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Status { get; set; }
+        public bool IsRegistered { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDelete { get; set; }
+        
+        public User(string firstName, string lastName, string status)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Status = status;
+        }
+    }
+}

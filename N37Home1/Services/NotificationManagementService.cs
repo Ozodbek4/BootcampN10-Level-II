9ae7dc4186a680b880f5b327c4a405c50b86ab42
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-09-18 19:41:55 +0500

    keepon

--- N37Home1/Services/NotificationManagementService.cs
+++ N37Home1/Services/NotificationManagementService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using N37Home1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,25 @@ namespace N37Home1.Services
 {
     internal class NotificationManagementService
     {
+        private readonly EmailSenderService _emailSenderService;
+        private readonly EmailService _emailService;
+        private readonly EmailTemplateService _emailTemplateService;
+        private readonly UserService _userService;
+
+        public NotificationManagementService(EmailSenderService emailSenderService, EmailService emailService, 
+            EmailTemplateService emailTemplateService, UserService userService)
+        {
+            _emailSenderService = emailSenderService;
+            _emailService = emailService;
+            _emailTemplateService = emailTemplateService;
+            _userService = userService;
+        }
+
         public void NotifyUsers()
         {
-
+            var users = _userService.GetUser(new );
+            var templates = _emailTemplateService.GetTemplates(users);
+            var message = _emailService.GetMassages(IEnumerable<(User, EmailTemplate, string)>x(users, templates, "anvarjonovozodbek416@gmail.com"));
         }
     }
 }
---
+++ N38Home2/Program.cs
@@ -0,0 +1,80 @@
+﻿
+using Bogus;
+
+int idSeed = 1;
+Faker<Employee> fake = new Faker<Employee>()
+    .RuleFor(employee => employee.Id, id => idSeed)
+    .RuleFor(employee => employee.Name, name => name.Person.FullName)
+    .RuleFor(employee => employee.WorkName, workName => workName.Company.CompanyName());
+
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string WorkName { get; set; }
+    
+    public Employee(int id, string name, string workName)
+    {
+        Id = id;
+        Name = name;
+        WorkName = workName;
+    }
+}
+
+public class Order
+{
+    public Guid Id { get; set; }
+    public string ProductName { get; set; }
+    public decimal Price { get; set; }
+    public DateTime OrderDate { get; set; }
+    
+    public Order(Guid id, string productName, decimal price, DateTime orderDate)
+    {
+        Id = id;
+        ProductName = productName;
+        Price = price;
+        OrderDate = orderDate;
+    }
+}
+
+public class UserAddress
+{
+    public string StreetName { get; set; }
+    public string CityName { get; set; }
+    public string CountryName { get; set; }
+    
+    public UserAddress(string streetName, string cityName, string countryName)
+    {
+        StreetName = streetName;
+        CityName = cityName;
+        CountryName = countryName;
+    }
+}
+
+public class BlogPost
+{
+    public string Title { get; set; }
+    public string Body { get; set; }
+    
+    public BlogPost(string title, string body)
+    {
+        Title = title;
+        Body = body;
+    }
+}
+
+public class WeatherReport
+{
+    public DateTime DateTime { get; set; }
+    public double AvarageTemperature { get; set; }
+    public string Condition { get; set; }
+
+    public WeatherReport(DateTime dateTime, double avarageTemperature, string condition)
+    {
+        DateTime = dateTime;
+        AvarageTemperature = avarageTemperature;
+        Condition = condition;
+    }
+
+}

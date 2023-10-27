e1566c09f63964cada8ca889fc807cd8a0456df1
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-10-28 00:43:50 +0500

    done N57 homeTask

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * N57 HomeTask
+ * 
+ * Savollar:
+ *  - external configuration ga kirishi mumkin bo'lgan qiymatlarga misollar keltiring
+    - configuration modelga yozishimiz mumkin bo'lgan ma'lumot tiplarini yozing
+ * 
+ * Javoblar:
+ *  1. - external configuration ga kirishi mumkin bo'lgan qiymatlarga misollar keltiring
+ * Configuration provider(appSetting.Json, appSettingDevelopment.Json, appSettingProvider.Json), Environment variable, User secret, Command Line Argument 
+ * 
+ * 
+ *  2. - configuration modelga yozishimiz mumkin bo'lgan ma'lumot tiplarini yozing
+ * Hamma configuration modellarni AddSingleto<T>, AddScopped<T>, AddTrensient. <T> - bu register qilinadigan servicelar hisoblanadi
+ * 
+ */

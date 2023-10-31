7b29e8cbbf68e405430c0fd85ecf08847e276bb1
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-11-01 03:49:26 +0500

    doing n65 hometask

---
+++ N65.Identity.Infrastructure/Common/Identity/Services/AccountService.cs
@@ -0,0 +1,17 @@
+﻿using N65.Identity.Application.Common.Identity.Services;
+using N65.Identity.Domain.Entities;
+
+namespace N65.Identity.Infrastructure.Common.Identity.Services;
+
+public class AccountService : IAccountService
+{
+    public ValueTask<User> CreateUserAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> VerificationAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ N65.Identity.Infrastructure/Common/Identity/Services/EmailOrchestrationService.cs
@@ -0,0 +1,11 @@
+﻿using N65.Identity.Application.Common.Notifications.Services;
+
+namespace N65.Identity.Infrastructure.Common.Identity.Services;
+
+public class EmailOrchestrationService : IEmailOrchestrationService
+{
+    public ValueTask<bool> SendMessageAsync(string emailAddress, string message)
+    {
+        throw new NotImplementedException();
+    }
+}
